908e78cfe7ff73e96d5ce42f2bd2fddd83105096
arya dradjica <arya@nlnetlabs.nl>
2026-01-19 09:49:17 +0100

    [tests/full] Introduce 'Daemon'
    'Daemon' can be used to reliably launch 'kmip2pkcs11' using SoftHSMv2
    from a regular integration test. With this little bit of setup effort,
    it eliminates the need for containerized testing, and allows multiple
    tests to execute in parallel with a single daemon.
    
    An initial attempt used the regular test harness and put the daemon in
    a 'LazyLock' static; but this would prevent dropping and cleaning up the
    temporary directory. Instead, the test harness was omitted, and tests
    will have to be parallelized manually.

--- tests/full.rs
+++ tests/full.rs
@@ -16,4 +16,148 @@
 // Only available on Unix machines.
 #![cfg(unix)]
 
-fn main() {}
+use std::{
+    io,
+    net::{Ipv6Addr, SocketAddr, TcpListener},
+    process::Command,
+};
+
+use command_fds::{CommandFdExt, FdMapping};
+
+/// A running `kmip2pkcs11` daemon.
+pub struct Daemon {
+    /// The address of the KMIP server.
+    address: SocketAddr,
+
+    /// The daemon process.
+    process: std::process::Child,
+
+    /// A pre-built slot (label, PIN, and SO PIN).
+    slot: (String, String, String),
+
+    /// The temporary directory holding the daemon's state.
+    tempdir: tempfile::TempDir,
+}
+
+impl Daemon {
+    /// Launch a new [`Daemon`].
+    pub fn launch() -> io::Result<Self> {
+        // Bind a TCP socket for the KMIP server.
+        let sock = TcpListener::bind((Ipv6Addr::LOCALHOST, 0))?;
+        let addr = sock.local_addr()?;
+
+        // Allocate a temporary directory for the daemon.
+        let tempdir = tempfile::tempdir()?;
+
+        // Determine the location of the SoftHSMv2 library.
+        // TODO: Allow overriding with env var / CLI.
+        let lib_path = "/usr/lib64/softhsm/libsofthsm2.so";
+
+        // Configure the daemon.
+        let config_path = tempdir.path().join("config.toml");
+        let log_path = tempdir.path().join("log");
+        // TODO: Use <https://crates.io/crates/indoc>?
+        std::fs::write(
+            &config_path,
+            format!(
+                r#"
+version = "v1"
+pkcs11.lib-path = {lib_path:?}
+daemon.log-level = "info"
+daemon.log-target = {{ type = "file", path = {log_path:?} }}
+"#
+            ),
+        )?;
+
+        // Configure SoftHSMv2.
+        let tokens_path = tempdir.path().join("tokens").display().to_string();
+        let hsm_config_path = tempdir.path().join("softhsm2.conf");
+        std::fs::write(
+            &hsm_config_path,
+            format!(
+                r#"
+directories.tokendir = {tokens_path}
+objectstore.backend = file
+log.level = DEBUG
+"#
+            ),
+        )?;
+
+        // Create the tokens directory.
+        std::fs::create_dir_all(&tokens_path)?;
+
+        // Initialize the HSM slot/token.
+        let label = String::from("kmip2pkcs11-test");
+        let so_pin = String::from("123456");
+        let pin = String::from("abcdef");
+        if !Command::new("softhsm2-util")
+            .args([
+                "--init-token",
+                "--free",
+                "--label",
+                &label,
+                "--so-pin",
+                &so_pin,
+                "--pin",
+                &pin,
+            ])
+            .env("SOFTHSM2_CONF", &hsm_config_path)
+            .spawn()?
+            .wait()?
+            .success()
+        {
+            return Err(io::Error::other("could not initialize SoftHSMv2"));
+        }
+
+        // Make sure the daemon binary is ready.
+        let project_dir = std::env::current_dir()?;
+        if !Command::new("cargo")
+            .args(["build", "--bin", "kmip2pkcs11"])
+            .spawn()?
+            .wait()?
+            .success()
+        {
+            return Err(io::Error::other("could not build 'kmip2pkcs11'"));
+        }
+
+        // Launch the daemon.
+        let daemon_path = project_dir.join("target/debug/kmip2pkcs11");
+        let proc = Command::new(&daemon_path)
+            .arg("--config")
+            .arg(&config_path)
+            .fd_mappings(vec![FdMapping {
+                parent_fd: sock.into(),
+                child_fd: 3,
+            }])
+            .unwrap()
+            .env("SOFTHSM2_CONF", &hsm_config_path)
+            .env("LISTEN_FDS", "1")
+            .current_dir(tempdir.path())
+            .spawn()?;
+
+        Ok(Self {
+            address: addr,
+            process: proc,
+            slot: (label, pin, so_pin),
+            tempdir,
+        })
+    }
+}
+
+impl Drop for Daemon {
+    fn drop(&mut self) {
+        // Stop the daemon.
+        // TODO: Use SIGTERM?
+        let _ = self.process.kill();
+    }
+}
+
+fn main() {
+    // Launch the daemon.
+    let daemon = Daemon::launch().unwrap_or_else(|err| {
+        eprintln!("Could not launch the daemon: {err}");
+        std::process::exit(1)
+    });
+
+    todo!()
+}

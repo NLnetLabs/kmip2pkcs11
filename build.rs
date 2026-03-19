19af7a26fcedcbec4054a1ee4edaa3a2f8c58198
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2026-03-19 09:25:12 +0100

    Review feedback: Remove unnnecessary copy-pasted complexity.
    Co-authored-by: Jannik Peters <jannik@nlnetlabs.nl>

--- build.rs
+++ build.rs
@@ -79,9 +79,7 @@ fn main() -> Result<(), ()> {
                     _ => "unknown error occured while attempting to run git",
                 };
                 println!("cargo::warning={msg}. Unable to determine git revision...");
-                if package == "cascade-hsm-bridge" {
-                    println!("cargo::rerun-if-changed=.git");
-                }
+                println!("cargo::rerun-if-changed=.git");
             })
             .ok()
             .map(|o| o.status.success());
